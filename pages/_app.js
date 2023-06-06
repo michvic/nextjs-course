20136a5ee6b6dc5391acf686d3b1553de43d3013
Michel Victor <michel.costa@cogna.com.br>
2023-06-05 23:06:00 -0300

    add analytics

--- pages/_app.js
+++ pages/_app.js
@@ -8,6 +8,19 @@ export default function MyApp({ Component, pageProps }) {
         <link rel="preconnect" href="https://fonts.googleapis.com"/>
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
         <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;1,300;1,400;1,500;1,600;1,700&display=swap" rel="stylesheet"/>
+        {/* <!-- Google tag (gtag.js) --> */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-QW0KTQZ9D3"></script>
+        <script 
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+
+              gtag('config', 'G-QW0KTQZ9D3');
+            `
+          }}  
+        />
       </Head>
       <GlobalStyle />
       <Component {...pageProps} />
